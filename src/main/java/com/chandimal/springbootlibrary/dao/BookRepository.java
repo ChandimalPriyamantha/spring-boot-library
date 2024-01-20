081f2c340bce061a351aa8988300dfadde270fd1
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2024-01-21 03:30:38 +0530

    Initial commit

---
+++ src/main/java/com/chandimal/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,9 @@
+package com.chandimal.springbootlibrary.dao;
+
+import com.chandimal.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    
+}
