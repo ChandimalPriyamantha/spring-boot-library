dd87f80e2df2b39ea7036ac919cc3d4698c670e2
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2024-01-23 01:42:37 +0530

    add findByTitleContaining

--- src/main/java/com/chandimal/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/chandimal/springbootlibrary/dao/BookRepository.java
@@ -1,9 +1,12 @@
 package com.chandimal.springbootlibrary.dao;
 
 import com.chandimal.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
