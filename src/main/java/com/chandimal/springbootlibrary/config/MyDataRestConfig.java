e70b4739d0984e8a9f77d56cf115cc908cb1ce2d
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2024-02-12 07:18:54 +0530

    add findByTitleContaining

--- src/main/java/com/chandimal/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/chandimal/springbootlibrary/config/MyDataRestConfig.java
@@ -2,6 +2,7 @@ package com.chandimal.springbootlibrary.config;
 
 
 import com.chandimal.springbootlibrary.entity.Book;
+import com.chandimal.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -23,9 +24,11 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class,config,theUnsupportedActions);
 
 
         /* Configure CORS Mapping */
